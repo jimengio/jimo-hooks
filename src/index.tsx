f07619a6b1b8525de14e02bcfe9d24a05e81030b
wangcch <ci713@qq.com>
2020-03-11 18:54:33 +0800

    feat: useDebouncedClick
    base on useAsyncClick

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,15 @@
+import useAsyncClick from "./useAsyncClick";
 import useDebounce from "./useDebounce";
 import useDebouncedCallback from "./useDebouncedCallback";
+import useDebounceClick from "./useDebouncedClick";
 import useDeepCompareCache from "./useDeepCompareCache";
 import useDeepEffect from "./useDeepEffect";
 
 export {
+  useAsyncClick,
   useDebounce,
   useDebouncedCallback,
+  useDebounceClick,
   useDeepCompareCache,
   useDeepEffect,
 };
---
+++ src/useDebouncedClick.ts
@@ -0,0 +1,32 @@
+import {} from "react";
+import useAsyncClick from "./useAsyncClick";
+import useDebouncedCallback from "./useDebouncedCallback";
+
+/**
+ * useDebouncedClick
+ *
+ * @param asyncFunc
+ * @param wait number @default 300
+ * @param options maxWait, leading, trailing
+ */
+export default function useDebouncedClick<O>(
+  asyncFunc: (o: O) => Promise<void>,
+  wait = 300,
+  options?: { maxWait?: number; leading?: boolean; trailing?: boolean }
+): [(o: O) => void, boolean, () => void, () => void] {
+  const [asyncClick, loading] = useAsyncClick<O>(asyncFunc);
+
+  const [
+    onClickEvent,
+    cancelDebouncedCallback,
+    callPending,
+  ] = useDebouncedCallback(
+    (args: O) => {
+      asyncClick(args);
+    },
+    wait,
+    options
+  );
+
+  return [onClickEvent, loading, cancelDebouncedCallback, callPending];
+}
