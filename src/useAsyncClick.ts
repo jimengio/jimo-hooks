c59d0aefc61cb4f637116e4da0133bed18aa8d9a
wangcch <ci713@qq.com>
2020-03-11 18:53:58 +0800

    feat: useAsyncClick

---
+++ src/useAsyncClick.ts
@@ -0,0 +1,28 @@
+import { useState, useCallback } from "react";
+
+/**
+ * useAsyncClick
+ *
+ * @param asyncFunc
+ */
+export default function useAsyncClick<O>(
+  asyncFunc: (o: O) => Promise<void>
+): [(o: O) => void, boolean] {
+  const [loading, setLoading] = useState(false);
+
+  const onAsyncEvent = useCallback(
+    async (options: O) => {
+      try {
+        setLoading(true);
+        await asyncFunc(options);
+      } catch (error) {
+        console.error("useAsyncFn loading error: %o", error);
+      } finally {
+        setLoading(false);
+      }
+    },
+    [setLoading, asyncFunc]
+  );
+
+  return [onAsyncEvent, loading];
+}
