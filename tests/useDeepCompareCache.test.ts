f40743bd4a6537fccd09b3078f0a2f3afd771616
wangcch <ci713@qq.com>
2020-03-13 14:46:22 +0800

    test: useDeepCompareCache, useDeepEffect

---
+++ tests/useDeepCompareCache.test.ts
@@ -0,0 +1,27 @@
+import { renderHook } from "@testing-library/react-hooks";
+
+import useDeepCompareCache from "../src/useDeepCompareCache";
+
+describe("useDeepCompareCache", () => {
+  let obj1 = { a: 1, b: { b1: 2 } };
+  let obj2 = { a: 1, b: { b1: 2 } };
+
+  it("Origin value", () => {
+    const { result } = renderHook(() => useDeepCompareCache(obj1));
+
+    expect(result.current).toEqual(obj1);
+  });
+
+  it("Compare data", () => {
+    const { result } = renderHook(() => useDeepCompareCache(obj1));
+
+    // the same data
+    expect(result.current).toEqual(obj2);
+
+    obj1.b.b1 = 0;
+    expect(result.current).toEqual(obj1);
+
+    obj2.b.b1 = -1;
+    expect(result.current).not.toEqual(obj2);
+  });
+});
---
+++ tests/useDeepEffect.test.ts
@@ -0,0 +1,61 @@
+import { useRef } from "react";
+import { renderHook } from "@testing-library/react-hooks";
+
+import useDeepEffect from "../src/useDeepEffect";
+
+describe("useDeepEffect", () => {
+  let obj1 = { a: 1, b: { b1: 2 } };
+  let obj2 = { a: 1, b: { b1: 2 } };
+  let obj3 = { a: 1, b: { b1: 0 } };
+
+  it("Same values", done => {
+    const { rerender } = renderHook(
+      ({ obj }) => {
+        const firstRenderRef = useRef(true);
+
+        useDeepEffect(() => {
+          if (firstRenderRef.current) {
+            firstRenderRef.current = false;
+          } else {
+            done.fail("Trigger the same value");
+          }
+        }, [obj]);
+      },
+      {
+        initialProps: { obj: obj1 },
+      }
+    );
+
+    // init
+    rerender();
+
+    rerender({ obj: obj1 });
+    rerender({ obj: obj2 });
+    done();
+  });
+
+  it("Different values", () => {
+    const { rerender } = renderHook(
+      ({ obj }) => {
+        const firstRenderRef = useRef(true);
+
+        useDeepEffect(() => {
+          if (firstRenderRef.current) {
+            firstRenderRef.current = false;
+            expect(obj).toEqual(obj1);
+          } else {
+            expect(obj).toEqual(obj3);
+          }
+        }, [obj]);
+      },
+      {
+        initialProps: { obj: obj1 },
+      }
+    );
+
+    // init
+    rerender();
+
+    rerender({ obj: obj3 });
+  });
+});
